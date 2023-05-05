c46b5e0dbece34ce2910e2a7dd419810425e1b90
alyaothman14 <alya.othman@contra.com>
2023-05-05 15:22:05 -0400

    test:add more browsers

--- testData/login_page_data.py
+++ testData/login_page_data.py
@@ -1,5 +1,5 @@
 class LoginPageData:
 
-    test_login_page_country_data=[
-        {"country":"Afghanistan","code":"+93" },
-        {"country":"Albania","code":"+355"}]
+    test_login_page_country_data = [
+        {"country": "Afghanistan", "code": "+93"},
+        {"country": "Albania", "code": "+355"}]
